149c8797c0d4e67a2c5f954dfdf64f9ad993b70b
vitamin777 <rlaalsdn8@gmail.com>
2023-01-07 17:25:31 +0900

    feat: SPA setting & 홈,소개,프로필 페이지 구축
    -src/pages 폴더에 Home,About,Profile.jsx file 생성
    -Home.jsx 에서 Link 컴포넌트 활용해 경로 설정
    -Profile.jsx 에서 더미데이터 data 객체 생성 후 useParam 을 사용해 URL 파라미터(username)에 접근해 프로필 조회하도록하고 존재하지않는다면 상황에 맞는 메시지 출력토록함
    -index.js 에서 router 를 사용하기위해 BrowserRouter 로 App.js 를 감싸줬음
    -App.js 에서 Routes,Route 활용해 경로 및 컴포넌트 요소 설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import {Link} from "react-router-dom";
+
 const Home = () => {
 	return (
 			<div>
 				<h1>Home</h1>
 				<p>가장 먼저 보여지는 페이지입니다.</p>
+				<ul>
+					<li>
+						<Link to="/about">소개</Link>
+					</li>
+					<li>
+						<Link to="/profile/developer">developer 의 프로필</Link>
+					</li>
+					<li>
+						<Link to="/profile/gildong">gildong 의 프로필</Link>
+					</li>
+					<li>
+						<Link to="/profile/void">존재하지 않는 프로필</Link>
+					</li>
+				</ul>
 			</div>
 	)
 }
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,36 @@
+import { useParams } from "react-router-dom";
+
+const data = {
+  developer: {
+    name: "김민우",
+    description: "리액트를 좋아하는 개발자",
+  },
+  gildong: {
+    name: "홍길동",
+    description: "고전 소설 홍길동전의 주인공",
+  },
+};
+
+const Profile = () => {
+  // useParams()를 사용하여 URL 파라미터에 접근 가능 (URL 파라미터는 문자열 형태)
+  const params = useParams();
+  /** data 객체에 프로필 정보들을 key-value 형태로 담아두었음
+   * Profile 컴포넌트에서는 username URL 파라미터를 통해 프로필을 조회한 뒤에 프로필이 존재하지 않는다면
+   * 존재하지않는 사용자라고 문구를 보여주고 존재한다면 프로필 정보를 보여주도록 함*/
+  const profile = data[params.username];
+
+  return (
+    <div>
+      <h1>사용자 프로필</h1>
+      {profile ? (
+        <div>
+          <h2>{profile.name}</h2>
+          <p>{profile.description}</p>
+        </div>
+      ) : (
+        <p>존재하지 않는 사용자입니다.</p>
+      )}
+    </div>
+  );
+};
+export default Profile;
